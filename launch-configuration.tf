c6352bcee4fae70b32d1c45202d74a43e77ccf1b
Shane Mulligan <mullikine@gmail.com>
2021-04-27 14:40:37 +1200

    Tue 27 Apr 2021 14:40:37 NZST

--- launch-configuration.tf
+++ launch-configuration.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "ecs-launch-configuration" {
   name                        = "ecs-launch-configuration"
   image_id                    = data.aws_ami.latest_ecs.id
-  instance_type               = "t2.medium"
+  instance_type               = "t2.micro"
   iam_instance_profile        = aws_iam_instance_profile.ecs-instance-profile.id
 
   root_block_device {
